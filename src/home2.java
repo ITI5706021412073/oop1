a8a2cff9f36c4edd4e8bb39e7532083910bc2072
ITI5706021412073 <5706021412073@fitm.kmutnb.ac.th>
2016-02-18 21:42:06 +0700

    ครั้งที่1
    เริ่มต้นประกาศตัวแปร

--- src/home2.java
+++ src/home2.java
@@ -1,6 +1,8 @@
-
+import java.io.*;
+import javax.swing.JOptionPane;
+import java.util.*;
 public class home2 {
-
+	int A,E,I,O,U;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
