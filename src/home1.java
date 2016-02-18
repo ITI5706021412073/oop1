e39aed122d2256b5cf0851def96d14aa1df01c3f
ITI5706021412073 <5706021412073@fitm.kmutnb.ac.th>
2016-02-18 21:37:45 +0700

    ครั้งที่1
    ประกาศตัวแปร

--- src/home1.java
+++ src/home1.java
@@ -1,6 +1,9 @@
-
+import java.io.*;
+import javax.swing.JOptionPane;
+import java.util.*;
 public class home1 {
-
+	
+	int Sec,Min,Hour,Day;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
