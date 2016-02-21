35694a64c183d624fcfe4e678cca29c7f9fd131b
ITI5706021412073 <5706021412073@fitm.kmutnb.ac.th>
2016-02-21 18:01:28 +0700

    ครั้งที่ 2
    รับค่าทางแป้นพิมพ์

--- src/home2.java
+++ src/home2.java
@@ -3,6 +3,12 @@ import javax.swing.JOptionPane;
 import java.util.*;
 public class home2 {
 	int A,E,I,O,U;
+	public String getData()
+	{
+		String name = JOptionPane.showInputDialog(null,"Enter name ","InputData",JOptionPane.QUESTION_MESSAGE);
+		name = name.toUpperCase();
+		return name;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
