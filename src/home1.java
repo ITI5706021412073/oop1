d3859fa62a517bcfdf876276f9585539e7756d34
ITI5706021412073 <5706021412073@fitm.kmutnb.ac.th>
2016-02-21 17:58:59 +0700

    ครั้งที่ 2
    รับค่า คำนวณ

--- src/home1.java
+++ src/home1.java
@@ -4,6 +4,18 @@ import java.util.*;
 public class home1 {
 	
 	int Sec,Min,Hour,Day;
+	public void getData()
+	{
+		Sec = Integer.parseInt(JOptionPane.showInputDialog(null,"Enter Sec","Input",JOptionPane.QUESTION_MESSAGE));
+	}
+	public void Calc()
+	{
+		Min = Sec / 60;
+		Sec = Sec % 60;
+		
+		Hour = Min / 60;
+		Min = Min % 60;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
