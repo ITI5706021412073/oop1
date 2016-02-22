a08139fb9ea0fdddd69ed5b13fc52866f8b14c07
ITI5706021412073 <5706021412073@fitm.kmutnb.ac.th>
2016-02-22 23:56:32 +0700

    ครั้งที่ 3
    คำนวณผลลัพท์ แสดงผล

--- src/home2.java
+++ src/home2.java
@@ -9,9 +9,44 @@ public class home2 {
 		name = name.toUpperCase();
 		return name;
 	}
+	public void Calc(String name)
+	{
+		A=E=I=O=U=0;
+		for(int i=0;i<name.length();i++)
+		{
+			if(name.charAt(i) == 'A')
+			{
+				A=A+1;
+			}
+			else if(name.charAt(i) == 'E')
+			{
+				E=E+1;
+			}
+			else if(name.charAt(i) == 'I')
+			{
+				I=I+1;
+			}
+			else if(name.charAt(i) == 'O')
+			{
+				O=O+1;
+			}
+			else if(name.charAt(i) == 'U')
+			{
+				U=U+1;
+			}
+		}
+	}
+	public void Print()
+	{
+		JOptionPane.showMessageDialog(null,"Have\n"+"A : "+A+"\nE : "+E+"\nI : "+I+"\nO : "+O+"\nU : "+U,"Message",JOptionPane.PLAIN_MESSAGE);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String name;
+		home2 ts2 = new home2();
+		name = ts2.getData();
+		ts2.Calc(name);
+		ts2.Print();
 	}
 
 }
