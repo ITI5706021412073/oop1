69f74957db607d348671d2a768eb636a776bb52a
ITI5706021412073 <5706021412073@fitm.kmutnb.ac.th>
2016-02-29 21:55:16 +0700

    Update home3.java

--- src/home3.java
+++ src/home3.java
@@ -1,4 +1,12 @@
+import java.io.*;
+import javax.swing.*;
 
-public class home3 {
-
+import java.util.*;
+public class home3 
+{
+	int [][] Sec3,Sec4,Sec5;
+	String[][] Sum;
+	int a=0;
+	Random rand = new Random();
+	
 }
